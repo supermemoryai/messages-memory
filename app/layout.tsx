15cf8615d29ac931aa0179a3cbe4049814923b20
Shreyans Jain <shreyans@shreyans.sh>
2025-05-11 15:56:14 +0530

    remove vercel button
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://supermemory.chat'),
+  title: 'Supermemory Infinite Chat',
+  description: 'Just ask.',
 };
 
 export const viewport = {
