e996d957050ae8df14f07968c9b8e5131f879ddf
Shreyans Jain <shreyans@shreyans.sh>
2025-05-14 01:24:07 +0530

    add favicon
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- middleware.ts
+++ middleware.ts
@@ -52,8 +52,8 @@ export const config = {
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - icon.png, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    '/((?!_next/static|_next/image|icon.png|sitemap.xml|robots.txt).*)',
   ],
 };
